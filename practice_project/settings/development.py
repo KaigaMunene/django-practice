42739e7878ed55bb635631bf79580c26dcf63b74
KaigaMunene <kaigamunene@gmail.com>
2022-03-04 09:34:18 +0300

    (mod): change allowed host
    * changed allowed host for development

--- practice_project/settings/development.py
+++ practice_project/settings/development.py
@@ -2,4 +2,4 @@ from practice_project.settings.base import ALLOWED_HOSTS
 
 DEBUG = True
 
-ALLOWED_HOSTS += ["https://practise-django-app.herokuapp.com/"]
+ALLOWED_HOSTS += [".herokuapp.com"]
--- practice_project/settings/production.py
+++ practice_project/settings/production.py
@@ -1,5 +1,3 @@
-from practice_project.settings.base import ALLOWED_HOSTS
-
 DEBUG = False
 
-ALLOWED_HOSTS += ["https://practise-django-app.herokuapp.com/"]
+ALLOWED_HOSTS = ["https://practise-django-app.herokuapp.com/"]
